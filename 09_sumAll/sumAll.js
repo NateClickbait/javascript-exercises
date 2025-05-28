e69f22991a2f8e8bf03d0425abcb704d4791c424
NateClickbait <andrewtruong40024@gmail.com>
2025-05-28 16:08:57 -0500

    completed exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof(num1) !== "number" || typeof(num2) !== "number" ||
+        Math.floor(num1) !== num1 || Math.floor(num2) != num2 ||
+        num1 < 0 || num2 < 0)
+        return "ERROR";
 
+    total = 0;
+    finish = (num1 > num2) ? num1 : num2;
+    start = (num1 < num2) ? num1 : num2;
+    for (i = start; i <= finish; ++i) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
