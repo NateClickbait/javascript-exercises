6388e3d9c9d1ab6814230c8695e9fb6e179b2e06
NateClickbait <andrewtruong40024@gmail.com>
2025-05-28 15:59:22 -0500

    completed exercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for (const removeNum of args) {
+        for (i = 0; i < arr.length; ++i) {
+            if (arr[i] === removeNum) {
+                arr.splice(i,1);
+                --i;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
