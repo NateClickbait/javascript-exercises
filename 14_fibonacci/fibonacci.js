e6893806b09396d0186f64b264c851b33b4a51c9
NateClickbait <andrewtruong40024@gmail.com>
2025-05-31 22:02:43 -0500

    completed exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(pos) {
+    if (pos < 0)
+        return "OOPS";
+    if (typeof pos === "string")
+        pos = +pos;
 
+    let num1 = 0;
+    let num2 = 1;
+    for (i = 0; i < pos; ++i) {
+        let temp = num1;
+        num1 = num2;
+        num2 += temp;
+    }
+
+    return num1;
 };
 
 // Do not edit below this line
