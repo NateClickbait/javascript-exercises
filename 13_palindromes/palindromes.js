5bf2614d2cb9957f180e0b172734ace369fe4763
NateClickbait <andrewtruong40024@gmail.com>
2025-05-31 21:52:52 -0500

    completed exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let startPos = 0;
+    let endPos = string.length - 1;
 
+    string = string.toLowerCase();
+
+    //learn regex
+    while (startPos < endPos) {
+        if (!(/^[a-zA-Z0-9]$/.test(string[startPos])))
+            ++startPos;
+        else if (!(/^[a-zA-Z0-9]$/.test(string[endPos])))
+            --endPos;
+        else if (string[startPos] !== string[endPos])
+            return false;
+        else {
+            ++startPos;
+            --endPos;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
